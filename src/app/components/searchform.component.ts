b5ed71602d3b5304ea6c8ceee54fd745a8f59823
dhananjay <dj+12@programmr.com>
2018-12-21 13:46:30 +0000

    FINISH TEST

--- src/app/components/searchform.component.ts
+++ src/app/components/searchform.component.ts
@@ -36,6 +36,7 @@ export class SearchformComponent implements OnInit {
             this.githubUser.user = false;
         }
     }
+    //THIS IS DJ FAILES
 
     getUserInformation() {
         if (this.githubUser.userName && this.githubUser.userName.length > 0) {
